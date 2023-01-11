da408eb1cd3b6b24321f9b458d432364caad9a9f
greynode <118746740+greynode@users.noreply.github.com>
2023-01-11 22:11:34 +0530

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/javascript/index.js',
+    entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '',
